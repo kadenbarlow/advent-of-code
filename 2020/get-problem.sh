d14d9b5122b49cb04cfe67073b808aa8892bb0ba
Kaden Barlow <kadenbarlow@gmail.com>
2020-12-10 07:49:57 -0700

    solution to day 10

--- 2020/get-problem.sh
+++ 2020/get-problem.sh
@@ -10,19 +10,24 @@ directory="day$1"
 mkdir -p $directory
 
 input_file="$directory/input.txt"
-if [[ ! -f $input_file ]]; then
-  curl "https://adventofcode.com/2020/day/$1/input" --output $input_file
-fi
+touch $input_file
+# if [[ ! -f $input_file ]]; then
+#   curl "https://adventofcode.com/2020/day/$1/input" --output $input_file
+# fi
+
+touch "$directory/sample.txt"
 
 problem_file="$directory/problem.rb"
 if [[ ! -f $problem_file ]]; then
   cat > $problem_file <<-EOF
 #! /usr/bin/env ruby
 
-def part1
-  f = File.open('./input.txt')
+f = File.open('./sample.txt')
+# f = File.open('./input.txt')
+
+def part1()
 end
 
-puts part1
+puts part1()
 EOF
 fi
